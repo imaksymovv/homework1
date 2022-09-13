199b86d45c8259182bbbda34a89d5617ddd7166b
imaksymovv <Ivan.Vasiliy.Maksymov@gmail.com>
2022-09-13 17:52:58 +0200

    added homework1

---
+++ someproject.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+struct answer {
+	int number[3][1];
+};
+
+void function(answer& r) {
+
+	r.number[2][0]++;
+
+	if (r.number[2][0] == 10) {
+		r.number[1][0]++;
+		r.number[2][0] = 0;
+	}
+	if (r.number[1][0] == 10) {
+		r.number[0][0]++;
+		r.number[1][0] = 0;
+	}
+	if (r.number[2][0] == r.number[1][0] || r.number[2][0] == r.number[0][0]) {
+		do {
+			r.number[2][0]++;
+			if (r.number[2][0] == 10) {
+				r.number[1][0]++;
+				r.number[2][0] = 0;
+			}
+			if (r.number[1][0] == r.number[0][0]) {
+				do {
+					r.number[1][0]++;
+				} while (r.number[1][0] == r.number[0][0]);
+			}
+			if (r.number[1][0] == 10) {
+				r.number[0][0]++;
+				r.number[1][0] = 0;
+			}
+		} while (r.number[2][0] == r.number[1][0] || r.number[2][0] == r.number[0][0]);
+	}
+}
+
+int main() {
+	bool c = false;
+	answer p;
+	p.number[0][0] = { 0 };
+	p.number[1][0] = { 0 };
+	p.number[2][0] = { -1 };
+	do {
+		function(p);
+		if (p.number[0][0] != 10) {
+			std::cout << p.number[0][0] << p.number[1][0] << p.number[2][0] << std::endl;
+		}
+		else {
+			c = true;
+		}
+
+	} while (!c);
+	return 0;
+}
